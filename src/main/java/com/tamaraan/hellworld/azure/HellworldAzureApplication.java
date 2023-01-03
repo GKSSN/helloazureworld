cb0582d8567dc137adb312e503f756bdeebe2657
unknown <gopal.ssn@gmail.com>
2023-01-03 11:49:58 +0100

    First Commit

---
+++ src/main/java/com/tamaraan/hellworld/azure/HellworldAzureApplication.java
@@ -0,0 +1,13 @@
+package com.tamaraan.hellworld.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellworldAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellworldAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamaraan/hellworld/azure/HellworldAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamaraan.hellworld.azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellworldAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
